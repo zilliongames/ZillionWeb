8470b7aef0c82c2a9da666ede2ab94b7512ffb6f
jhlee <jhlee@zilliongames.net>
2026-04-11 00:01:09 +0900

    feat: 1차 홈페이지 완성

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "play-lh.googleusercontent.com" },
+      { protocol: "https", hostname: "img.youtube.com" },
+    ],
+  },
 };
 
 export default nextConfig;
